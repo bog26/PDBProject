c183adcc29be0e20f125780a9bae932af9bc5f81
bog26 <l_bogdan2001@yahoo.com>
2021-09-02 23:21:15 +0200

    new classes and model folder

---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+
+
+namespace PDBProject.Models
+{
+    public class Product
+    {
+        //[Key]
+        public int Id { get; set;}
+
+        [Required]
+        public string Name { get; set;}
+        
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set;}
+    }
+}
